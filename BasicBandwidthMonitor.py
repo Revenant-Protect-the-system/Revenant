427e9bd04094f4521631e9be67dcc4f631eb5e20
Joshdunny <joshdunny2005@gmail.com>
2025-02-04 11:22:31 +0000

    Fixed Spelling

--- BasicBandwidthMonitor.py
+++ BasicBandwidthMonitor.py
@@ -10,15 +10,15 @@ while True:
     bytes_sent = psutil.net_io_counters().bytes_sent
     bytes_total = bytes_received + bytes_sent
 
-    new_reveived = bytes_received - last_received
+    new_received = bytes_received - last_received
     new_sent = bytes_sent - last_sent
     new_total = bytes_total - last_total
 
-    mb_new_reveived = new_reveived / 1024 / 1024
+    mb_new_received = new_received / 1024 / 1024
     mb_new_sent = new_sent / 1024 / 1024
     mb_new_total = new_total / 1024 / 1024
 
-    print(f"{mb_new_reveived:.2f} MB Recieved, {mb_new_sent:.2f} MB Sent, {mb_new_total:.2f} MB Total")
+    print(f"{mb_new_received:.2f} MB Recieved, {mb_new_sent:.2f} MB Sent, {mb_new_total:.2f} MB Total")
 
     last_received = bytes_received
     last_sent = bytes_sent
