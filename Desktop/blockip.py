c23f0aac18c2765aa56af905a87409ee0a00822c
dayheim <davidboylea@gmail.com>
2025-05-14 20:13:23 +0100

    delete uncessary files

--- Desktop/blockip.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def block_ip(ip):
-    
-    command = f"netsh advfirewall firewall add rule name=\'Block {ip}' dir=in action = block remoteip={ip}"
-    subprocess.run(command, shell=True) 
-    print(f"Ip blocked! IP: {ip}")
-
